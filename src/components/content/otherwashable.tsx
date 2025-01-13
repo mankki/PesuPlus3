b93127c68146e3986ff2191b87f1eaddf41b6b02
mankki <114470120+mankki@users.noreply.github.com>
2025-01-13 12:07:07 +0200

    Rivinvaihto nahkakäsittelykohtaan

--- src/components/content/otherwashable.tsx
+++ src/components/content/otherwashable.tsx
@@ -13,7 +13,7 @@ const otherwashable = () => {
             alt="nahkakaluste" />
             <h5 className="mb-2 text-xl font-bold leading-tight text-black">NAHKAKALUSTEET</h5>
               <p className="mb-4 text-2xl text-black text-center">
-                Pesemme ja hoitokäsittelemme nahkakalusteet käsin.
+                Pesemme ja hoitokäsittelemme <br> nahkakalusteet käsin.
               </p>
         </div>
         <div className = "flex flex-col rounded-lg sm:shrink-0 sm:grow sm:basis-0 sm:rounded-e-none items-center">
