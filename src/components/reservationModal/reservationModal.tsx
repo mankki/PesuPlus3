646dccc5acb29651b6d03c71e4b0f2f16992816a
mankki <114470120+mankki@users.noreply.github.com>
2025-02-07 12:49:24 +0200

    Update reservationModal.tsx

--- src/components/reservationModal/reservationModal.tsx
+++ src/components/reservationModal/reservationModal.tsx
@@ -12,10 +12,10 @@ const ReservationModal = (props: any) => {
     <>
       {modalState && (
         <>
-          <div className="justify-center items-center flex fixed inset-0 z-50 outline-none focus:outline-none overflow-hidden">
-            <div className="relative my-6 mx-auto w-full h-full">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full h-full bg-white outline-none focus:outline-none">
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+          <div className="justify-center items-center flex fixed inset-0 z-50 outline-none focus:outline-none overflow-hidden bg-white h-screen">
+            <div className="relative mx-auto w-full h-full">
+              <div className="border-0 shadow-lg relative flex flex-col w-full h-full outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200">
                   <h3 className="text-3xl font-semibold">Ajanvaraus</h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black text-3xl leading-none font-semibold outline-none focus:outline-none"
